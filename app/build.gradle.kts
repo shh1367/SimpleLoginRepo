5db5c92fa11a5ada97d5c995a1223bbcc96c77de
newHamed <shh1367@yahoo.com>
2024-04-26 22:38:03 +0330

    feature : add dataBinding feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+        buildFeatures {
+            dataBinding =true;
+        }
+
+
+
 }
+
+
 
 dependencies {
 
