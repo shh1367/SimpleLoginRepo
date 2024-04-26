a83a8aa17b1d5bf63fd23bd75593b8ffa092badd
newHamed <shh1367@yahoo.com>
2024-04-27 00:27:55 +0330

    feature : add user class

---
+++ app/src/main/java/com/example/simplelogin/model/User.java
@@ -0,0 +1,16 @@
+package com.example.simplelogin.model;
+
+public class User {
+    private String phoneNumber;
+
+    public User(String phoneNumberHint) {
+        this.phoneNumberHint = phoneNumberHint;
+    }
+
+    public User(){
+
+    }
+
+    private String phoneNumberHint;
+
+}
