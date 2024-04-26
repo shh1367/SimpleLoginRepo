a1ff25b466f6ad481b76b616bd6fafb316bcfd6f
newHamed <shh1367@yahoo.com>
2024-04-27 01:12:08 +0330

    feature :rename main_activity.xml

--- app/src/main/java/com/example/simplelogin/ActivityGetNumber.java
+++ app/src/main/java/com/example/simplelogin/ActivityGetNumber.java
@@ -9,6 +9,6 @@ public class ActivityGetNumber extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_get_number);
     }
 }
