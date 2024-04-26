d68adb6953128e22ee916a42060eb8c0023b745a
newHamed <shh1367@yahoo.com>
2024-04-27 00:55:45 +0330

    feature :add UserModel class

--- app/src/main/java/com/example/simplelogin/viewmodel/UserModel.java
+++ app/src/main/java/com/example/simplelogin/viewmodel/UserModel.java
@@ -1,2 +1,33 @@
-package com.example.simplelogin.viewmodel;public class UserModel {
+package com.example.simplelogin.viewmodel;
+
+import com.example.simplelogin.model.User;
+
+public class UserModel {
+    private String phoneNumber ;
+
+    private String phoneNumberHint;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public UserModel(User user){
+        this.phoneNumberHint = user.phoneNumberHint;
+
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumberHint() {
+        return phoneNumberHint;
+    }
+    public void setPhoneNumberHint(String phoneNumberHint) {
+        this.phoneNumberHint = phoneNumberHint;
+    }
+
+
+
+
 }
