8bae7b17f2212d597f0be60127dbfe1226a0cf2a
newHamed <shh1367@yahoo.com>
2024-04-28 00:18:05 +0330

    feature:add mySinglton class to make single Volley object

--- app/src/main/java/com/example/simplelogin/viewmodel/UserModel.java
+++ app/src/main/java/com/example/simplelogin/viewmodel/UserModel.java
@@ -1,33 +1,27 @@
 package com.example.simplelogin.viewmodel;
 
+import androidx.databinding.BaseObservable;
+
+import com.example.simplelogin.R;
 import com.example.simplelogin.model.User;
 
-public class UserModel {
-    private String phoneNumber ;
+public class UserModel extends BaseObservable {
+    private String phoneNumber;
 
-    private String phoneNumberHint;
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public UserModel(User user){
-        this.phoneNumberHint = user.phoneNumberHint;
+    public UserModel(User user) {
+        this.phoneNumber = user.phoneNumber;
 
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+        notifyPropertyChanged(R.id.edt_phone);
     }
-
-    public String getPhoneNumberHint() {
-        return phoneNumberHint;
-    }
-    public void setPhoneNumberHint(String phoneNumberHint) {
-        this.phoneNumberHint = phoneNumberHint;
-    }
-
-
 
 
 }
